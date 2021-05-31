cef1641b7b673d2fb0915a17e322e65c230bbfe6
Matías Locatti <42481638+goldenx86@users.noreply.github.com>
2021-05-30 21:49:38 -0300

    Corrigiendo nombres de funciones para respetar ejemplos

--- tp4_ej3.py
+++ tp4_ej3.py
@@ -4,18 +4,18 @@
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
 
-def convert_to_retarded_units(celsius):
+def convertir_a_fahrrenheit(celsius):
     """Convertir a unidades de países especiales"""
     retarded = (celsius * (9/5)) + 32
     return retarded
 
-def convert_to_celsius(retarded):
+def convertir_a_centigrados(retarded):
     """Convertir a unidades con lógica y sentido"""
     celsius = (retarded -32) * (5/9)
     return celsius
 
 if __name__ == "__main__":
     temp = int(input("Ingrese la temperatura a convertir en unidades especiales: "))
-    print(convert_to_retarded_units(temp))
+    print(convertir_a_fahrrenheit(temp))
     temp = int(input("Ingrese la temperatura a convertir en grados celsius: "))
-    print(convert_to_celsius(temp))
+    print(convertir_a_centigrados(temp))
