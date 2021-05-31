5684beadd47db723c624f7e94b98b6a59bb93e7a
Matías Locatti <42481638+goldenx86@users.noreply.github.com>
2021-05-30 21:52:52 -0300

    Update tp4_ej9.py

--- tp4_ej9.py
+++ tp4_ej9.py
@@ -5,32 +5,32 @@
 ################
 
 
-def primo(numero):
+def es_primo(numero):
     """Revisa si un número es primo"""
     if numero > 1:
         for i in range(2, int(numero/2) + 1):
             if numero % i != 0:
-                es_primo = True
+                primo = True
             else:
-                es_primo = False
+                primo = False
     else:
-        es_primo = False
+        primo = False
 
     if numero < -1:
         numero = (numero * -1)
         for i in range(2, int(numero / 2) + 1):
             if numero % i != 0:
-                es_primo = True
+                primo = True
             else:
-                es_primo = False
+                primo = False
     else:
-        es_primo = False
+        primo = False
 
-    if es_primo:
+    if primo:
         print("El número es primo.")
     else:
         print("El número no es primo.")
 
 if __name__ == "__main__":
     num = int(input("Ingrese el número: "))
-    print(primo(num))
+    print(es_primo(num))
